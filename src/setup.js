aec260be304830934edec580e05dbaade8ed2c9a
jaygraffagna <jaygraffagna10@gmail.com>
2019-03-24 14:32:19 -0500

    remove install into setup

--- src/setup.js
+++ src/setup.js
@@ -23,9 +23,17 @@ module.exports = ()=>{
 		});
 	}
 
+	//Creates a package.json for app startups
+	if(!fs.existsSync('package.json')){
+		fs.writeFile('package.json', '{"scripts":{ "start": "node node_modules/file-drop-image-resizer/src/index" }}', (err)=>{
+			if(err)
+				console.log("package file already created");
+		});
+	}
+
 	//Creates a command file for windows to start with a file
 	if(os.platform == 'win32'){
-		fs.writeFile(config.path + '/start.cmd', "npm image-resizer start", (err)=>{
+		fs.writeFile(config.path + '/start.cmd', "cd " + process.cwd() + "\n npm start", (err)=>{
 			if(err)
 				console.log("start up file already created");
 		});
